897f53ad8c101699b06b2c869cdb0aa1207e7c90
3414 <3414@example.com>
2018-10-10 11:48:48 +0900

    リレーションを貼りました

--- app/models/kennkoukiroku.rb
+++ app/models/kennkoukiroku.rb
@@ -1,2 +1,7 @@
 class Kennkoukiroku < ApplicationRecord
+ has_many :comments
+ belongs_to :byouketu
+ belongs_to :tikoku
+ belongs_to :gakunenn
+ belongs_to :seito
 end
--- app/models/seito.rb
+++ app/models/seito.rb
@@ -1,2 +1,4 @@
 class Seito < ApplicationRecord
+ has_many :kennkoukirokus
+ belongs_to :gakunenn
 end
