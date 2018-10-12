a9695a3cae66eae9d34808731f7b7e4ef0504d8b
3414 <3414@example.com>
2018-10-12 11:35:52 +0900

    バリデーション

--- app/models/kennkoukiroku.rb
+++ app/models/kennkoukiroku.rb
@@ -1,7 +1,12 @@
 class Kennkoukiroku < ApplicationRecord
  has_many :comments
- belongs_to :byouketu
- belongs_to :tikoku
+ belongs_to :byouketu,optional: true
+ belongs_to :tikoku,optional: true
  belongs_to :gakunenn
  belongs_to :seito
+
+ validates :hiduke,presence: true
+ validates :gakunenn_id,presence: true
+ validates :seito_id,presence: true
+
 end
--- app/models/seito.rb
+++ app/models/seito.rb
@@ -1,4 +1,11 @@
 class Seito < ApplicationRecord
  has_many :kennkoukirokus
  belongs_to :gakunenn
+
+ validates :gakunenn_id,presence:true
+ validates :banngou,presence:true
+ validates :seibetu,presence:true
+ validates :simei,presence:true 
+
+ validates :banngou,numericality: { greater_than_or_equal_to: 0 }
 end
