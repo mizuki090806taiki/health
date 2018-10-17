e2151ba2077e96f967920d7009bcd0fe594c9fda
3414 <3414@example.com>
2018-10-17 12:12:51 +0900

    日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   resources :seitos
   resources :kennkoukirokus
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'home#top'
 end
