9ca648283531110bcaae907a411d9669f17c9227
3409 <3409@example.com>
2018-10-26 11:41:51 +0900

    認証機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  before_action :basic_authenticate
+
+  private
+
+  def basic_authenticate
+    authenticate_or_request_with_http_basic do |user, pass|
+      if user == 'kyouyu' && pass == 'kyouyu'
+        session[:username] = "養護教諭" 
+        session[:userkengen] = 1
+        true
+      else
+        if user == 'seito' && pass == 'seito'
+          session[:username] = "生徒"
+          session[:userkengen] = 2
+          true
+        else
+          if user == 'sensei' && pass == 'sensei'
+            session[:username] = "先生"
+            session[:userkengen] = 3
+            true
+          else
+        false
+          end
+        end  
+      end  
+  end
+end
 end
