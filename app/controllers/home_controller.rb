81751f51fe180bacd8d5b699eeea5669de6349dc
3414 <3414@example.com>
2018-12-12 11:11:34 +0900

    集計機能の追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def top
+
+@b = Byouketu.all
+@ninzu = []
+   @b.each do |b|
+    @ninzu[b.id] = Kennkoukiroku.where("byouketu_id = ?",b.id).count
+   end 
+ 
+@k = []
+@k = Kennkoukiroku.where("hiduke = ? and kesseki = ?",Date.today,true).count
+  
+     
   end
 end
